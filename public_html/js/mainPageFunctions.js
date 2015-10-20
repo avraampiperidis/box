3b470e9a3cefa5c087f9e6b16051458d22ef6b90
Abraham Piperidis <piperidis3@yahoo.gr>
2015-10-21 00:11:14 +0300

    metakinisa ta javascript apo mainuserpage.php se ksexoristo js arxeio

---
+++ public_html/js/mainPageFunctions.js
@@ -0,0 +1,53 @@
+/**
+ * Created by abraham on 21/10/2015.
+ */
+
+
+var showtablestatus = 0;
+
+function gotomainpage() {
+    window.location.href = "index.php";
+}
+
+function logOut() {
+    $.ajax({
+        url: 'logout.php?argument=logOut',
+        success: function(data) {
+            window.location.href = data;
+        }
+    })
+}
+
+function showTable() {
+    if(showtablestatus == 0) {
+        var table = document.getElementById("table");
+        table.style.display = "table";
+        showtablestatus = 1;
+    } else if(showtablestatus == 1) {
+        var table = document.getElementById("table");
+        table.style.display = "none";
+        showtablestatus = 0;
+    }
+}
+
+function createFolder() {
+    $(document).ready(function(){
+        var  userfolder =prompt("enter folder name");
+        if(userfolder){
+            $.ajax({
+                url: 'createfolder.php?argument=createfolder&foldername='+userfolder,
+                success: function(res){
+                    if(res == "success") {
+                        window.location.href = "usermainpage.php";
+                    } else {
+                        toastr.error("invalid folder name, or try again later","Error");
+                    }
+                }
+            });} });
+}
+
+function loadFolder(path) {
+    //
+}
+
+
--- public_html/usermainpage.php
+++ public_html/usermainpage.php
@@ -94,57 +94,8 @@ class Page {
      <script src="../resources/jquery/jquery-2.1.4.min.js"></script>
      <script src="../resources/jquery/toastr.min.js"></script>
 
-    <script>
+     <script src="js/mainPageFunctions.js"></script>
 
-        var showtablestatus = 0;
-
-        function gotomainpage() {
-            window.location.href = "index.php";
-        }
-
-        function logOut() {
-            $.ajax({
-                url: 'logout.php?argument=logOut',
-                success: function(data) {
-                    window.location.href = data;
-                }
-            })
-        }
-
-        function showTable() {
-            if(showtablestatus == 0) {
-                var table = document.getElementById("table");
-                table.style.display = "table";
-                showtablestatus = 1;
-            } else if(showtablestatus == 1) {
-                var table = document.getElementById("table");
-                table.style.display = "none";
-                showtablestatus = 0;
-            }
-        }
-
-        function createFolder() {
-            $(document).ready(function(){
-                var  userfolder =prompt("enter folder name");
-                if(userfolder){
-                    $.ajax({
-                        url: 'createfolder.php?argument=createfolder&foldername='+userfolder,
-                        success: function(res){
-                            if(res == "success") {
-                                window.location.href = "usermainpage.php";
-                            } else {
-                                toastr.error("invalid folder name, or try again later","Error");
-                            }
-                        }
-                    });} });
-        }
-
-        function loadFolder(path) {
-            //
-        }
-
-
-    </script>
 
     <?php
     }
