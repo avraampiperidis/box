667376c853584ba4dd263920863a0e83d8eead5a
Abraham Piperidis <piperidis3@yahoo.gr>
2015-11-08 22:29:12 +0200

    local

--- resources/config.php
+++ resources/config.php
@@ -4,7 +4,7 @@
 //keep in safe place ,outside apache's web root directory
 $dbaccess["host"] = "127.0.0.1";
 $dbaccess["username"] = "root";
-$dbaccess["password"] = "1zeronerone";
+$dbaccess["password"] = "password";
 
 $dbaccess["database"][0] = "box";
 
